113104161c771774b1d6bbbc678a14aaeee75176
Kunal Deotale <kdeotale4@gmail.com>
2023-06-22 23:04:28 +0530

    frist commit

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-red-500">Open Ai Project</h1>
+    </div>
+  );
+}
+
+export default App;
